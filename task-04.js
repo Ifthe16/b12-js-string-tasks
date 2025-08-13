ed1cddf3d7b093c91401467a51294aeadb9d72bd
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-13 19:37:04 +0600

    added task-04.js file

---
+++ task-04.js
@@ -0,0 +1,9 @@
+/* Task-04 */
+const sentence = "Xylophones and foxes exist in the box";
+
+console.log(sentence);
+
+
+const newSentence = sentence.replace(/x/g, 'y').replace(/X/g, 'Y');
+
+console.log(newSentence);
