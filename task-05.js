3ec5223771b4e48c6dd12025d4d8a48b3d649b1b
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-13 20:14:18 +0600

    updated task-05.js file

--- task-05.js
+++ task-05.js
@@ -1,9 +1,9 @@
 /* Task-05 */
-const sentence = "hello world this is a javascript practice";
+const sentence = "hello world this is a javascript practice.";
 const capitalizedWords = [];
 
 const words = sentence.split(' ');
-console.log(words);
+// console.log(words);
 
 for (let word of words) {
     const firstLetter = word[0].toUpperCase();
